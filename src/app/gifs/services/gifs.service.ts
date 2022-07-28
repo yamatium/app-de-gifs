8e6748b91af8f8e454518bd534c8c44e700ee590
Rodrigo Fleitas <rodrigofleitas910@hotmail.com>
2022-07-28 17:02:44 -0300

    changed to 20 gif limit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -42,7 +42,7 @@ export class GifsService {
 
     const params = new HttpParams()
           .set('api_key', this.apiKey)
-          .set('limit', '10')
+          .set('limit', '20')
           .set('q', query);
 
     
